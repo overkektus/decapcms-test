53995a1ac94f1f36fb09e1f9aa6125cc47828892
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-01 13:06:29 +0200

    fix page

--- app/page.js
+++ app/page.js
@@ -1,25 +1,25 @@
 import Image from 'next/image'
 import Head from "next/head"
-import { attributes, react as HomeContent } from '../content/home.md'
+// import { attributes, react as HomeContent } from '../content/home.md'
 
 export default function Home() {
-  let { title, cats } = attributes
+  // let { title, cats } = attributes
   return (
     <>
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
       <article>
-        <h1>{title}</h1>
+        {/* <h1>{title}</h1> */}
         <HomeContent />
-        <ul>
+        {/* <ul>
           {cats.map((cat, k) => (
             <li key={k}>
               <h2>{cat.name}</h2>
               <p>{cat.description}</p>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </article>
     </>
   )
